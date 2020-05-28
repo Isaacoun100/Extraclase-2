f21f3d478d8975975a224d15c7071d7fe736ee93
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-05-27 20:10:18 -0600

    updated-first-encryptor-class
    Added the source code and respective referencing for the MD5 encryption class for the Facade Implementation.

--- src/com/itcr/datos/extraclass/Facade/MD5Encryptor.java
+++ src/com/itcr/datos/extraclass/Facade/MD5Encryptor.java
@@ -1,4 +1,31 @@
 package com.itcr.datos.extraclass.Facade;
+/*
+The implementation of this class below is based on
+the retrievable article on: https://examples.javacodegeeks.com/core-java/java-facade-design-pattern-example/
+by author Abhishek Kothari. Adapted to this assignment by Alejandro Quesada, claiming no rights over the source code in this class.
 
+ */
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 public class MD5Encryptor {
+    public String encrypt(String text) {
+        String hash = "";
+        try {
+            MessageDigest msgDigest = MessageDigest.getInstance("MD5");
+            msgDigest.update(text.getBytes());
+
+            byte textBytes[] = msgDigest.digest();
+
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < textBytes.length; i++) {
+                buffer.append(Integer.toString((textBytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            hash = buffer.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return hash;
+    }
 }
