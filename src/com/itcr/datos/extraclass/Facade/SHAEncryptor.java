c0004707a5ca4caea9d126a101ad2ed2f83c42d4
Alejandro Quesada Calderón <ale11199@estudiantec.cr>
2020-05-27 20:13:58 -0600

    updated-sha-encryption-class
    Added the source code and the respective referencing for the Class code, with only a minor name change by the programmer to better match the followed order as of now. Further source code to be added so the encryptors can be implemented to the facade and thus allow the user to encrypt messages.

--- src/com/itcr/datos/extraclass/Facade/SHAEncryptor.java
+++ src/com/itcr/datos/extraclass/Facade/SHAEncryptor.java
@@ -1,4 +1,29 @@
 package com.itcr.datos.extraclass.Facade;
+/*
+The implementation of this class below is based on
+the retrievable article on: https://examples.javacodegeeks.com/core-java/java-facade-design-pattern-example/
+by author Abhishek Kothari. Adapted to this assignment by Alejandro Quesada, claiming no rights over the source code in this class.
+ */
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class SHAEncryptor {
+    public String encrypt(String text) {
+        String hash = "";
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA");
+            byte[] textBytes = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+
+            StringBuffer buffer = new StringBuffer();
+            for (int i = 0; i < textBytes.length; i++) {
+                buffer.append(Integer.toString((textBytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            hash = buffer.toString();
+        }
+        catch(NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return hash;
+    }
 }
